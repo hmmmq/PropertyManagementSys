206af086de66b6a58cca1cbf4262f234a62d35db
hmmmq <1053825066@qq.com>
2024-11-06 19:16:01 +0800

    Update shujuku.sql

--- 物业管理系统/sql/shujuku.sql
+++ 物业管理系统/sql/shujuku.sql
@@ -16,6 +16,27 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `admin`
+--
+
+DROP TABLE IF EXISTS `admin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `gender` varchar(45) DEFAULT NULL,
+  `phone` varchar(45) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `type` tinyint(1) DEFAULT '1',
+  `email` varchar(45) DEFAULT NULL,
+  `balance` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `car`
 --
 
@@ -30,6 +51,25 @@ CREATE TABLE `car` (
   `park` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `contract`
+--
+
+DROP TABLE IF EXISTS `contract`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `contract` (
+  `id` int(11) NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `sign_userid` int(11) DEFAULT NULL,
+  `sign_username` varchar(255) DEFAULT NULL,
+  `type` varchar(45) DEFAULT NULL,
+  `sign_time` datetime DEFAULT CURRENT_TIMESTAMP,
+  `status` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -83,8 +123,45 @@ CREATE TABLE `house` (
   `area` int(11) DEFAULT NULL,
   `address` varchar(255) DEFAULT NULL,
   `status` varchar(255) DEFAULT NULL,
+  `enter_time` varchar(45) DEFAULT NULL,
+  `leave_time` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `inform`
+--
+
+DROP TABLE IF EXISTS `inform`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `inform` (
+  `id` int(11) NOT NULL,
+  `userid` int(11) DEFAULT NULL,
+  `message` varchar(255) DEFAULT NULL,
+  `inform_time` datetime DEFAULT CURRENT_TIMESTAMP,
+  `status` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `park`
+--
+
+DROP TABLE IF EXISTS `park`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `park` (
+  `id` int(11) NOT NULL,
+  `carid` int(11) DEFAULT NULL,
+  `create_time` datetime DEFAULT CURRENT_TIMESTAMP,
+  `username` varchar(255) DEFAULT NULL,
+  `userid` int(11) DEFAULT NULL,
+  `status` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -117,4 +194,4 @@ CREATE TABLE `user` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-11-04 17:17:55
+-- Dump completed on 2024-11-06 19:15:51
